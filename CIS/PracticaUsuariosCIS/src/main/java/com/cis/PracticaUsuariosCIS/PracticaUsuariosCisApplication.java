2cfd2c95afde8de46b7be08ec1ff3883e7727797
MartinezSergio <soysergiom@gmail.com>
2018-11-21 15:30:42 +0100

    inicio proyecto

---
+++ CIS/PracticaUsuariosCIS/src/main/java/com/cis/PracticaUsuariosCIS/PracticaUsuariosCisApplication.java
@@ -0,0 +1,12 @@
+package com.cis.PracticaUsuariosCIS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaUsuariosCisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaUsuariosCisApplication.class, args);
+	}
+}
---
+++ CIS/PracticaUsuariosCIS/src/test/java/com/cis/PracticaUsuariosCIS/PracticaUsuariosCisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cis.PracticaUsuariosCIS;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PracticaUsuariosCisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
